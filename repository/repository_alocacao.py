ddac2b79d8b9bad165941fd7f21b7a524b7de1ab
thyezoliveira <thyezoliveira@gmail.com>
2026-08-07 16:08:25 -0300

    feat(alocacao): Estrutura basica de alocaçao de equipamentos implementada
    - Mudança realizada no schema da alocaçao para que contenha uma lista de
    ids.
    - Repositorio de alocaçoes construido.
    - Service de alocaçoes construido.
    - Constroller de alocaçoes construido.
    - Registro da rota no main.

---
+++ repository/repository_alocacao.py
@@ -0,0 +1,37 @@
+from repository.database import Database
+from domain.schemas.schemas import Criar_alocacao
+
+
+class Alocacao_repository:
+    def __init__(self):
+        self._table_name = "tb_alocacao_equip"
+        self._database = Database()
+        self.supabase = self._database.obter_conexao()
+
+    def criar_alocacao(self, dados: Criar_alocacao) -> dict:
+        resposta = (
+            self.supabase.table(self._table_name).insert(dados.model_dump()).execute()
+        )
+        return resposta.data[0]
+
+    def obter_alocacoes(self) -> list:
+        resposta = self.supabase.table(self._table_name).select("*").execute()
+        return resposta.data
+
+    def obter_alocacao(self, alocacao_id: int) -> dict:
+        resposta = (
+            self.supabase.table(self._table_name)
+            .select("*")
+            .eq("alocacao_id", alocacao_id)
+            .execute()
+        )
+        return resposta.data[0]
+
+    def atualizar_status_alocacao(self, alocacao_id: int, novo_status: int) -> dict:
+        resposta = (
+            self.supabase.table(self._table_name)
+            .update({"status_alocacao": novo_status})
+            .eq("alocacao_id", alocacao_id)
+            .execute()
+        )
+        return resposta.data[0]
